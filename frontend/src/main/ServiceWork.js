b35f107dcf00ee58c496962a0448395204f165a4
willianjr <willianrochajunior@outlook.com>
2021-11-30 23:44:59 -0300

    ajuste erro

--- frontend/src/main/ServiceWork.js
+++ frontend/src/main/ServiceWork.js
@@ -5,33 +5,6 @@ const isLocalhost = Boolean(
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 )
-
-export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
-    if (publicUrl.origin !== window.location.origin) {
-      return
-    }
-
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-
-      if (isLocalhost) {
-        checkValidServiceWorker(swUrl, config)
-
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA',
-          )
-        })
-      } else {
-        registerValidSW(swUrl, config)
-      }
-    })
-  }
-}
-
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
@@ -68,7 +41,6 @@ function registerValidSW(swUrl, config) {
       console.error('Error during service worker registration:', error)
     })
 }
-
 function checkValidServiceWorker(swUrl, config) {
   fetch(swUrl, {
     headers: { 'Service-Worker': 'script' },
@@ -88,6 +60,31 @@ function checkValidServiceWorker(swUrl, config) {
     .catch(() => {
       console.log('No internet connection found. App is running in offline mode.')
     })
+}
+export function register(config) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href)
+    if (publicUrl.origin !== window.location.origin) {
+      return
+    }
+
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl, config)
+
+        navigator.serviceWorker.ready.then(() => {
+          console.log(
+            'This web app is being served cache-first by a service ' +
+              'worker. To learn more, visit https://bit.ly/CRA-PWA',
+          )
+        })
+      } else {
+        registerValidSW(swUrl, config)
+      }
+    })
+  }
 }
 
 export function unregister() {
