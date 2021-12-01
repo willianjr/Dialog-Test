d57f5781623fcfac04bcd90ebff15b6700142892
willianjr <willianrochajunior@outlook.com>
2021-11-30 23:03:02 -0300

    service work

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/App'
+import * as serviceWorker from './main/ServiceWork'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 )
+
+serviceWorker.register()
