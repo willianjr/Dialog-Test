4ffe6418b066f70be9ff686a11e85a9053b5b2e5
willianjr <willianrochajunior@outlook.com>
2021-12-01 00:11:23 -0300

    ajuste erro

--- frontend/src/common/components/Header/index.tsx
+++ frontend/src/common/components/Header/index.tsx
@@ -17,7 +17,7 @@ const Header = (): JSX.Element => {
     <Rows align={`center`}>
       <Col md={{ span: 2, offset: 1 }} sm={12}>
         <Link to="/">
-          <Logo src={Images.logos.vertical} />
+          <Logo alt={`Logomarca`} src={Images.logos.vertical} />
         </Link>
       </Col>
       <Col md={{ span: 7, offset: 1 }} sm={12}>
@@ -31,7 +31,11 @@ const Header = (): JSX.Element => {
             }}
             aria-label="search a friend"
           />
-          <ButtonSearch disabled={search === ''} variant="outline-primary" onClick={() => handleSearch()}>
+          <ButtonSearch
+            aria-label="Pesquisar"
+            disabled={search === ''}
+            variant="outline-primary"
+            onClick={() => handleSearch()}>
             <RiUserSearchFill />
           </ButtonSearch>
         </InputGroup>
